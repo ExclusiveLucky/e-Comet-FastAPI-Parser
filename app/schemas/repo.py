e1932208338a58e1800d48cf2aa78d1ba3259ca2
Denis Sidorov <99333313+ExclusiveLucky@users.noreply.github.com>
2024-09-18 15:22:27 +0300

    Create repo.py

---
+++ app/schemas/repo.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class RepoSchema(BaseModel):
+    """
+    Pydantic-схема для валидации данных о репозитории.
+    """
+    repo_name: str
+    owner: str
+    position_cur: int
+    position_prev: int
+    stars: int
+    watchers: int
+    forks: int
+    open_issues: int
+    language: str
+
+    class Config:
+        orm_mode = True
